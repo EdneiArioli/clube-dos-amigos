52d20809d877d6138ef3272d560deaa7b5ee1caf
edneiarioli <edneiarioli@yahoo.com>
2024-03-05 15:05:31 -0300

    primeiro comit projeto clube dos amigos

---
+++ scripits.js
@@ -0,0 +1,12 @@
+const form = document.querySelector(".formulario-fale-conosco")
+
+function mostrarform (){
+    form.style.left = "50%"
+    form.style.transform = "translatex(-50%)"
+   
+}
+function esconderform (){
+    form.style.left = "-330%"
+    form.style.transform = "translatex(0)"
+   
+}
